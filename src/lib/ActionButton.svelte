3c1e9e06f515d638e46688b422444a07244a054f
principle105 <principle105@gmail.com>
2022-12-28 00:34:58 -0500

    fix?

--- src/lib/ActionButton.svelte
+++ src/lib/ActionButton.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     export let href: string;
-    export let colour: string;
+    export let colour: "primary" | "secondary";
     export let size: "sm" | "md" | "lg" = "md";
 
     let text = size == "sm" ? "text-md" : size === "md" ? "text-lg" : "text-xl";
@@ -10,13 +10,18 @@
             : size === "md"
             ? "px-9 py-3"
             : "px-10 py-3.5";
+
+    let colours =
+        colour == "primary"
+            ? "bg-primary shadow-primary/20"
+            : "bg-secondary shadow-secondary/20";
 </script>
 
 <a
     {href}
     rel="noreferrer"
     target="_blank"
-    class="{text} {padding} bg-{colour} shadow-lg shadow-{colour}/20 text-zinc-50 rounded-full hover:-translate-y-1 hover:brightness-125 transition-all delay-75"
+    class="{text} {padding} {colours} shadow-lg text-zinc-50 rounded-full hover:-translate-y-1 hover:brightness-125 transition-all delay-75"
 >
     <slot />
 </a>
