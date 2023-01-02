73e88c0a789882340ed34d9a3bc2e9d8516a5420
principle105 <principle105@gmail.com>
2023-01-02 14:07:19 -0500

    fixed embed width

--- src/lib/Embed.svelte
+++ src/lib/Embed.svelte
@@ -3,25 +3,31 @@
     export let description: string = null;
 </script>
 
-<div
-    class="bg-discord-600 rounded-lg p-4 border-l-[6.5px] border-theme-primary w-max max-w-md"
->
-    <div class="flex justify-between gap-3">
-        <h3 class="text-base text-zinc-50 font-semibold basis-full">{title}</h3>
-        {#if $$slots.thumbnail}
-            <div class="basis-16">
-                <div class="float-right h-16 w-16 rounded-lg overflow-hidden">
-                    <slot name="thumbnail" />
+<div class="rounded-lg overflow-hidden">
+    <div
+        class="bg-discord-600 p-4 border-l-[5px] border-theme-primary w-max max-w-md"
+    >
+        <div class="flex justify-between gap-3">
+            <h3 class="text-base text-zinc-50 font-semibold basis-full">
+                {title}
+            </h3>
+            {#if $$slots.thumbnail}
+                <div class="basis-16">
+                    <div
+                        class="float-right h-16 w-16 rounded-lg overflow-hidden"
+                    >
+                        <slot name="thumbnail" />
+                    </div>
                 </div>
+            {/if}
+        </div>
+        {#if description !== null}
+            <p>{description}</p>
+        {/if}
+        {#if $$slots.image}
+            <div class="mt-4 max-w-md rounded-md overflow-hidden">
+                <slot name="image" />
             </div>
         {/if}
     </div>
-    {#if description !== null}
-        <p>{description}</p>
-    {/if}
-    {#if $$slots.image}
-        <div class="mt-4 max-w-md rounded-md overflow-hidden">
-            <slot name="image" />
-        </div>
-    {/if}
 </div>
