d52e25f12f9fbc7a3cdc66fdb6a08d5100507362
principle105 <principle105@gmail.com>
2023-01-02 19:19:20 -0500

    better footr

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,19 @@
-<footer class="py-9 px-24 flex gap-5 items-center w-screen">
+<footer
+    class="py-9 px-16 md:px-20 flex md:flex-row flex-col gap-5 items-center w-screen"
+>
     <img
         src="https://i.imgur.com/BIzs17V.png"
         alt="logo"
         class="h-20 rounded-full"
     />
-    <div>
+    <div class="text-center sm:text-left">
         <h1 class="text-zinc-50 text-3xl font-semibold">wordPractice</h1>
         <p class="text-zinc-400">Copyright &copy; 2023 wordPractice</p>
     </div>
     <div class="grow text-zinc-400">
-        <div class="float-right flex gap-11">
+        <div
+            class="float-right flex sm:flex-row flex-col gap-3 sm:gap-11 text-center"
+        >
             <a href="/">Rules</a>
             <a href="/">Privacy</a>
             <a href="/">Server</a>
