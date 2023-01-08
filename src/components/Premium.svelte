f2fd3c25d5ba1855fda2aed393e4e2fd5b9ba6ec
principle105 <principle105@gmail.com>
2023-01-08 04:13:53 -0500

    fixed spelling

--- src/components/Premium.svelte
+++ src/components/Premium.svelte
@@ -48,7 +48,7 @@
                 "Create custom typing test themes",
                 "Export test scores as CSV or JSON files",
                 "Reduced command cooldowns",
-                "Heatmap of typing accuracy",
+                "Heat Map of typing accuracy",
                 "Role on our Discord server",
             ]}
             colour="indigo"
